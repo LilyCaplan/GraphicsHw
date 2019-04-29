a5852cb98a20109023f05118825e2ad836de076d
lily <lrcgal@gmail.com>
2019-04-29 17:53:01 -0400

    changing conditional

--- CS472.skel/hw3/HW3b.cpp
+++ CS472.skel/hw3/HW3b.cpp
@@ -388,7 +388,7 @@ HW3b::resetMesh()
                 vec.setZ((!((i > m_grid/3 && j > m_grid/3)&&(i < m_grid*2/3 && j < m_grid*2/3))) ? .5f : 0.0f);
 				break;
 			case DIAGONALWALL:
-                vec.setZ((((m_grid-i)-j<2) && ((m_grid-i)-j>0)) ? .5f : 0.0f);
+                vec.setZ((((m_grid-i)-j<3) && ((m_grid-i)-j>0)) ? .5f : 0.0f);
 				break;
 			case SIDEWALL:
                 vec.setZ((i==1) ? .5f : 0.0f);
